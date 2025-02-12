ee08d2bb47e75a3063aab1c22f66c7760b99d53a
AkashMian1 <skylord1267@gmail.com>
2025-02-12 11:36:28 -0500

    test

--- Demo.java
+++ Demo.java
@@ -1,7 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
-    Vehicle.add(new MountainBike(1, 5, "Air Suspension", "disc Brake", "green"));
-    Vehicle.add(new );
-
+    MountainBike m = new MountainBike(1, 5, "Air Suspension", "disc Brake", "green");
+    
     }
 }
