91201e46f800691f7719c1925fa333e19da0c1fd
AkashMian1 <skylord1267@gmail.com>
2025-02-27 09:10:54 -0500

    htrhyhhh

--- Demo.java
+++ Demo.java
@@ -1,6 +1,10 @@
 public class Demo {
     public static void main(String[] args) {
     MountainBike m = new MountainBike(1, 5, "Air Suspension", "disc Brake", "green");
-    
+    RoadBike r = new RoadBike (1, 6, "Rim Brakes", "black");
+    Cars c = new Cars(4, 150, "white");
+    Helicopter h = new Helicopter(5, 20000, 6, "blue");
+    Plane p = new Plane( 165, 35000, 3, "Silver");
+    Submarine s = new Submarine (40, 4000, 2, "Nuclear", "black");
     }
 }
